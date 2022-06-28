29a97f504c1c5817a77f8bab6c02c17c7c83e9e1
Alex <alex.lin@nextfortune.tw>
2022-06-28 19:53:52 +0800

    remove redundant code

--- WalkingATM.Publisher/Program.cs
+++ WalkingATM.Publisher/Program.cs
@@ -15,49 +15,6 @@ using WalkingATM.Publisher.Strategies;
 using WalkingATM.Publisher.Utils;
 using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
-// var builder = WebApplication.CreateBuilder(args);
-//
-// LogManager.Configuration = new NLogLoggingConfiguration(builder.Configuration.GetSection("NLog"));
-// builder.Logging.ClearProviders();
-// builder.Logging.SetMinimumLevel(LogLevel.Trace);
-// builder.Host.UseNLog();
-//
-// builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-// builder.Services.AddOptions<AppSettings>().Bind(builder.Configuration);
-// builder.Services.AddSingleton<ITimeProvider, ITimeProvider>();
-// builder.Host.ConfigureContainer<ContainerBuilder>(
-//     containerBuilder =>
-//     {
-//         containerBuilder.RegisterAssemblyTypes(typeof(Program).Assembly)
-//             .Where(t => t.IsAssignableTo<IHostedService>())
-//             .As<IHostedService>()
-//             .WithAttributeFiltering()
-//             .SingleInstance();
-//
-//         // add new strategy:
-//         // 1. create new strategy push and stop job
-//         // 2. create new strategy class
-//         // 3. add new strategy value to enum
-//         // 4. add KeyFilter to new strategy push stop job
-//         foreach (var strategyEnum in Enum.GetValues<StrategyEnum>())
-//         {
-//             containerBuilder.RegisterType<LogFileMonitor>()
-//                 .Keyed<ILogFileMonitor>(strategyEnum)
-//                 .SingleInstance();
-//
-//             containerBuilder.RegisterAssemblyTypes(typeof(Program).Assembly)
-//                 .Where(t => t.IsAssignableTo<IStrategy>())
-//                 .Where(t => t.Name.StartsWith(strategyEnum.ToString()))
-//                 .Keyed<IStrategy>(strategyEnum)
-//                 .SingleInstance();
-//         }
-//
-//         containerBuilder.RegisterType<CronTimerFactory>()
-//             .As<ICronTimerFactory>()
-//             .InstancePerLifetimeScope();
-//     });
-
-
 var builder2 = Host.CreateDefaultBuilder(args)
     .UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureServices(
