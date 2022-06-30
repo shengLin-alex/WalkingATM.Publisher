579a95ccb636e9a0cde905be72bdd8ae36474fc6
Alex <alex.lin@nextfortune.tw>
2022-06-30 11:36:38 +0800

    add OpeningRisingPushJobTests

---
+++ WalkingATM.PublisherTests/ArgumentMatcherExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
+
+namespace WalkingATM.PublisherTests;
+
+public static class ArgumentMatcherExtensions
+{
+    /// <summary>
+    /// check with strict ordering as default
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="expected"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static bool ShouldEqual<T>(this T source, T expected)
+    {
+        return ShouldEqual(source, expected, options => options.WithStrictOrdering());
+    }
+
+    private static bool ShouldEqual<T>(
+        this T source,
+        T expected,
+        Func<EquivalencyAssertionOptions<T>,
+            EquivalencyAssertionOptions<T>> config)
+    {
+        try
+        {
+            source.Should().BeEquivalentTo(expected, config);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+}
